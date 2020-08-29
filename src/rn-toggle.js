be25358d63dd9857b39ea127250565867e517c5e
Abuda <xsitin@gmail.com>
2020-08-29 02:04:28 +0100

    added extra props

--- src/rn-toggle.js
+++ src/rn-toggle.js
@@ -1,19 +1,36 @@
 import React, { useRef, useEffect } from 'react';
-import { TouchableOpacity, StyleSheet, Animated, Easing } from 'react-native';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+  Easing,
+  Text,
+  View,
+} from 'react-native';
 
 const Toggle = ({
+  containerStyle = {},
+  textStyle = {},
   isActive = false,
+  text = null,
   toggle = () => null,
+  disabled = false,
+  vertical = false,
   width = 50,
-  circleColor = '#FFFFFF',
+  cursorColor = '#FFFFFF',
   borderColor = '#EEEEEE',
-  activeColor = '#333333',
+  activeColor = '#4CD862',
   inactiveColor = '#DDDDDD',
-  duration = 100,
+  duration = 150,
 }) => {
-  const activeColorRGB = hexToRGB(activeColor);
-  const inactiveColorRGB = hexToRGB(inactiveColor);
+  const activeColorRGB = disabled
+    ? hexToRGB(LightenDarkenColor(activeColor, 40))
+    : hexToRGB(activeColor);
+  const inactiveColorRGB = disabled
+    ? hexToRGB(LightenDarkenColor(inactiveColor, 40))
+    : hexToRGB(inactiveColor);
   const padding = width / 15;
+  const additionalWidth = text === null ? 0 : text.length * 6;
 
   const bgValue = useRef(new Animated.Value(0)).current;
   const bgColor = bgValue.interpolate({
@@ -27,7 +44,7 @@ const Toggle = ({
   const translateXValue = useRef(new Animated.Value(0)).current;
   const translateX = translateXValue.interpolate({
     inputRange: [0, 1],
-    outputRange: [padding, width / 2 + padding],
+    outputRange: [padding, width / 2 + padding + additionalWidth],
   });
 
   useEffect(() => {
@@ -47,23 +64,49 @@ const Toggle = ({
   }, [isActive]);
 
   return (
-    <TouchableOpacity onPress={toggle} activeOpacity={1}>
+    <TouchableOpacity
+      onPress={disabled ? null : toggle}
+      activeOpacity={1}
+      style={{
+        ...containerStyle,
+        transform: [{ rotate: vertical ? '270deg' : '0deg' }],
+      }}
+    >
       <Animated.View
         style={{
           borderRadius: width / 2,
           borderColor: borderColor,
           backgroundColor: bgColor,
-          borderWidth: StyleSheet.hairlineWidth,
-          width: width,
+          borderWidth: 1,
+          width: text ? width + additionalWidth : width,
           height: width / 2,
           flexDirection: 'row',
           alignItems: 'center',
         }}
       >
+        {text !== null && (
+          <View
+            style={{
+              ...StyleSheet.absoluteFill,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text
+              style={{
+                ...textStyle,
+                transform: [{ rotate: vertical ? '180deg' : '0deg' }],
+                fontSize: 12,
+              }}
+            >
+              {text}
+            </Text>
+          </View>
+        )}
         <Animated.View
           style={{
             borderRadius: width / 2 - padding * 2,
-            backgroundColor: circleColor,
+            backgroundColor: cursorColor,
             width: width / 2 - padding * 2,
             height: width / 2 - padding * 2,
             transform: [{ translateX }],
@@ -93,6 +136,34 @@ const hexToRGB = (h) => {
   }
 
   return [r, g, b];
+};
+
+const LightenDarkenColor = (col, amt) => {
+  var usePound = false;
+
+  if (col[0] == '#') {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  var num = parseInt(col, 16);
+
+  var r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00ff) + amt;
+
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  var g = (num & 0x0000ff) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
 };
 
 export default Toggle;
